6e0bc5d61881ed9fbe7c6d26eb47432cbcd0eb72
Shyam Stha <sthask2019@gmail.com>
2022-12-10 15:12:31 +0545

    Typescript classes and Tuples

--- index.js
+++ index.js
@@ -29,3 +29,48 @@
 //     LEGS = 4
 //   }
 //   console.log(BattleMenu.MAGIC, Equipment.HEAD); 
+//Typescript Generics
+// const getPartyLeader = <T>(memberList: T[]) => {
+//     return memberList[0];
+//   };
+//   const partyLeader = getPartyLeader(partyA); // Player
+//Tuples in typescript
+// type Weapon = {
+//     name: string;
+//     damage: number;
+//   };
+//   type Shield = {
+//     name: string;
+//     def: number;
+//   };
+//   const sword: Weapon = {
+//     name: "Onion Sword",
+//     damage: 10
+//   };
+//   const shield: Shield = {
+//     name: "Rusty Shield",
+//     def: 5
+//   };
+//   let equipment: [Weapon, Shield, boolean];
+//   equipment = [sword, shield, true]; // OK
+//   equipment[2] = false; // OK
+//   equipment = [shield, sword, false]; // Error
+//   equipment[1] = true; //Error
+//Classes in Typescript
+// class Job {
+//     public name: string;
+//     private level: number;
+//     readonly isExpansion: boolean;
+//     constructor(name: string, level: number, isExpansion: boolean) {
+//       this.name = name;
+//       this.level = level;
+//       this.isExpansion = isExpansion;
+//     }
+//   }
+//   const whiteMage = new Job("White Mage", 75, false);
+//   console.log(whiteMage.name); // "White Mage"
+//   console.log(whiteMage.level); // Error
+//   console.log(whiteMage.isExpansion); // false
+//   whiteMage.name = "Blue Mage"; // Ok
+//   whiteMage.level = 50; // Error
+//   whiteMage.isExpansion = true; // Error
