82c4e3fa9aa25ea4efdde1fb7c694ab1fd205be5
Shyam Stha <sthask2019@gmail.com>
2022-12-10 15:02:48 +0545

    Enums in typescript

--- index.js
+++ index.js
@@ -12,3 +12,20 @@
 //     console.log(`Attacking to ${target.name}`);
 //   }; // Error
 //   let attackResult = attack(lizard)
+//Typescript Enums
+// Enums are auto-indexed by default – both statements in the example above are equivalent.
+// enum BattleMenu {
+//     ATTACK,
+//     MAGIC,
+//     ABILITIES,
+//     ITEMS,
+//     DISENGAGE
+//   }
+//   enum Equipment {
+//     WEAPON = 0,
+//     HEAD = 1,
+//     BODY = 2,
+//     HANDS = 3,
+//     LEGS = 4
+//   }
+//   console.log(BattleMenu.MAGIC, Equipment.HEAD); 
